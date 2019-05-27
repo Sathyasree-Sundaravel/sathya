e3ad7f517b63f0158dbe8725b2a5dc9df0658689
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2019-05-27 20:46:46 +0530

    Create Check_whether_the_character_is_alphabet_or_not.py

---
+++ Check_whether_the_character_is_alphabet_or_not.py
@@ -0,0 +1,8 @@
+#Check whether the character is alphabet or not
+ch=input()
+if(ch>="a" and ch<="z"):
+    print("Alphabet")
+elif(ch>="A" and ch<="Z"):
+    print("Alphabet")
+else:
+   print("No")
