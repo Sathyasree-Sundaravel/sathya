ed379d86d30ae971f9eedc2495b13576a5138bad
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2019-05-29 12:41:04 +0530

    Create Factorial_of_a_number.py

---
+++ Factorial_of_a_number.py
@@ -0,0 +1,11 @@
+#Factorial of a number
+a=int(input())
+Factorial=1
+if(a<0):
+  print("Invalid")
+elif(a==0):
+  print(1)
+else:
+  for i in range(1,a+1):
+    Factorial=Factorial*i
+  print(Factorial)
