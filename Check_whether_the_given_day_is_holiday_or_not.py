2666f99f75429b037638216169a0c2f6d72a4c7a
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2019-06-09 17:34:09 +0530

    Create Check_whether_the_given_day_is_holiday_or_not.py

---
+++ Check_whether_the_given_day_is_holiday_or_not.py
@@ -0,0 +1,6 @@
+#Check whether the given day is holiday or not
+a=input() 
+if(a=='Saturday' or a=='Sunday'):
+  print('yes')
+elif(a=='Monday'or a=='Tuesday'or a=='Wednesday'or a=='Thursday'):
+  print('no')
