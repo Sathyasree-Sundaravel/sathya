bb8d71da6912e4db087792cbe6e035339f3008ee
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2019-06-09 17:25:45 +0530

    Create Check_whether_the_two_string_is_differ_by_only_one_character.py

---
+++ Check_whether_the_two_string_is_differ_by_only_one_character.py
@@ -0,0 +1,10 @@
+#Check whether the two string is differ by only one character
+s1,s2=input().split()
+count=0
+for i in range(len(s1)):
+    if s1[i]!=s2[i]:
+        count+=1
+if count==1:
+    print("yes")
+else:
+    print("no")
