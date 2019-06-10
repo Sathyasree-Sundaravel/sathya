59dc16d40cdb327f19c80a2ac2f42daf61be04e2
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2019-06-10 17:10:35 +0530

    Create Check_whether_the_given_three_points_lie_on_the_same_line_or_not.py

---
+++ Check_whether_the_given_three_points_lie_on_the_same_line_or_not.py
@@ -0,0 +1,12 @@
+#Check whether the given three points lie on the same line
+x,a=map(int,input().split())
+y,b=map(int,input().split())
+z,c=map(int,input().split())
+if(x==y==z):
+    print("yes")
+elif(a==b==c):
+    print("yes")
+elif(x==a and y==b and z==c):
+    print("yes")
+else:
+    print("no")
