e38b0f0c77ec30ca1de92bb5008e680a7d762845
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2019-06-10 16:55:17 +0530

    Update Find_the_smallest_number_which_is_greater_than_N_with_the_same_digits.py

--- Find_the_smallest_number_which_is_greater_than_N_with_the_same_digits.py
+++ Find_the_smallest_number_which_is_greater_than_N_with_the_same_digits.py
@@ -5,6 +5,9 @@ L=[]
 p=permutations(a)
 for i in p:
     b=int("".join(i))
+    if b<int(a):
+        r="impossible"
     if b>int(a):
         L.append(int("".join(i)))
-print(sorted(L)[0])
+        r=sorted(L)[0]
+print(r)
