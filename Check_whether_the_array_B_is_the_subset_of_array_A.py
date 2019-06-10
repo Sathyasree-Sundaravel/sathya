3814e0c46ea1c74063466492f40a6b3b32280741
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2019-06-10 11:57:17 +0530

    Create Check_whether_the_array_B_is_the_subset_of_array_A.py

---
+++ Check_whether_the_array_B_is_the_subset_of_array_A.py
@@ -0,0 +1,12 @@
+#Check whether the array B is the subset of array A
+N,M=map(int,input().split())
+A=list(map(int,input().split()))
+B=list(map(int,input().split()))
+count=0
+for i in range(0,len(B)):
+	if B[i] in A:
+		count+=1
+if count==len(B):
+	print("YES")
+else:
+	print("NO")
