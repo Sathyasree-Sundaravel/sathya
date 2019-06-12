4c618dd1b6cb6807dadc2c2bccf67b2025db3fff
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2019-06-12 15:57:31 +0530

    Create Check_whether_the_given_number_is_composite.py

---
+++ Check_whether_the_given_number_is_composite.py
@@ -0,0 +1,10 @@
+#Check whether the given number is composite
+a=int(input())
+count=0
+for i in range(1,a+1):
+    if(a%i==0):
+        count+=1
+if(count!=2):
+    print("yes")
+else:
+    print("no")
