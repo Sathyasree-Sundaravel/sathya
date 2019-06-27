8481a26164515a09c388457fc860ce4a551247e0
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2019-06-27 20:54:00 +0530

    Create Dhoni_to_destroy_all_tanks.py

---
+++ Dhoni_to_destroy_all_tanks.py
@@ -0,0 +1,15 @@
+#Dhoni to destroy all tanks
+n = int(input())
+b = []
+a = n//2 + n
+for i in range(1,n+1):
+  if i%2==0:
+    b.append(i)
+for i in range(1,n+1):
+  if i%2!=0:
+    b.append(i)
+for i in range(1,n+1):
+  if i%2==0:
+    b.append(i)
+print(a)
+print(*b)
