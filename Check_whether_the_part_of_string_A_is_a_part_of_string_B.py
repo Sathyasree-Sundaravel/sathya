31d3327904c496048899f4043dadc7018b404237
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2019-06-27 20:43:36 +0530

    Create Check_whether_the_part_of_string_A_is_a_part_of_string_B.py

---
+++ Check_whether_the_part_of_string_A_is_a_part_of_string_B.py
@@ -0,0 +1,11 @@
+#Check whether the part of string A is a part of string B
+a,b=map(str,input().split())
+count=0
+for i in range(0,len(a)):
+    for j in range(0,len(b)):
+        if a[i]==b[j]:
+            count+=1
+if count>=2:
+    print("yes")
+else:
+    print("no")
