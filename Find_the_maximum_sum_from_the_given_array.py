a8896a57d5a1d6999fed4119e9f2162098d40b77
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2019-06-30 18:07:13 +0530

    Create Find_the_maximum_sum_from_the_given_array.py

---
+++ Find_the_maximum_sum_from_the_given_array.py
@@ -0,0 +1,12 @@
+#Find the maximum sum from the given array
+n=int(input())
+s=list(map(int,input().split()))
+L=[]
+d=0
+for i in range(0,n-2):
+  for j in range(i,n,2):
+    d=d+s[j]
+  L.append(d)
+  d=0
+L.sort()
+print(L[-1])
